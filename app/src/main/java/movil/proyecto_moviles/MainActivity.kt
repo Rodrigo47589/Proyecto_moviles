ab868e419fe649c549fbb70aa0cc08fff23ee97a
LeonardoCam <89529410+LeonardoCam@users.noreply.github.com>
2026-06-30 21:32:33 -0500

    Update MainActivity.kt

--- app/src/main/java/movil/proyecto_moviles/MainActivity.kt
+++ app/src/main/java/movil/proyecto_moviles/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import movil.proyecto_moviles.screens.ContenidoPrincipal
 import movil.proyecto_moviles.ui.theme.Proyecto_movilesTheme
 
+//Class
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
